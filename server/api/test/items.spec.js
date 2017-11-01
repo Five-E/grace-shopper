b35e5aa7e5cfb2126acbe0cab7c8d89e3c0cdf6e
Eric Garnett <egcodes@gmail.com>
2017-11-01 16:33:02 -0400

    write items fetch test

---
+++ server/api/test/items.spec.js
@@ -0,0 +1,41 @@
+/* global describe beforeEach it */
+
+const {expect} = require('chai')
+const request = require('supertest')
+const db = require('../../db')
+const app = require('../../index')
+const Item = db.model('item')
+
+describe('Item routes', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('/api/items/', () => {
+
+    beforeEach(() => {
+      return Item.create({
+        name: 'Dwayne TRJ',
+        price: 39999.99,
+        stock: 1
+      })
+    })
+
+    it('GET /api/items', () => {
+      return request(app)
+        .get('/api/items/')
+        .expect(200)
+        .then(res => {
+          expect(res.body).to.be.an('array')
+          expect(res.body[0].name).to.be.equal('Dwayne TRJ')
+          expect(res.body[0].price).to.be.equal(39999.99)
+          expect(res.body[0].stock).to.be.equal(1)
+        })
+    })
+  })
+
+
+
+
+
+})
