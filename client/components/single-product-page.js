2ea80b08aa7a23ef59d8ad5ceeb0972215bf663c
NicholasGalarza <nicogala93@gmail.com>
2017-11-02 11:22:10 -0400

    locating all individual files

--- client/components/single-product-page.js
+++ client/components/single-product-page.js
@@ -5,67 +5,67 @@ import { connect } from 'react-redux'
  * COMPONENT
  */
 export const SingleProductPage = (props) => {
-   
-    const id = props.match.params.itemsId, 
-        targetItem = props.items.find(item => item.id === +id); 
 
-    if (!targetItem) return <div>Loading...</div>; 
+	const id = props.match.params.itemsId,
+		targetItem = props.items.find(item => item.id === +id);
 
-    const { name, picture, price, stock, description } = targetItem;
-    
-    return (
-        <div>
-            <img src={picture} />
-            <h3>{name}</h3>
-            <p>Description: {description}</p>
-            <p>Price:       ${price}</p>
-            <p>In Stock:    {stock}</p>
+	if (!targetItem) return <div>Loading...</div>;
 
-            <form onSubmit={handleSubmit}>
-                <select>
-                    {quantityDropdown(stock)}
-                </select>
-                <button>Add to Cart</button>
-            </form>
+	const { name, picture, price, stock, description } = targetItem;
 
-            <button onClick={returnButton}>Return to List</button>
-            <h1>Reviews Placeholder</h1>
-            {/* Create Reviews List component */}
-        </div>
-    );
+	return (
+		<div>
+			<img src={picture} />
+			<h3>{name}</h3>
+			<p>Description: {description}</p>
+			<p>Price:       ${price}</p>
+			<p>In Stock:    {stock}</p>
+
+			<form onSubmit={handleSubmit}>
+				<select>
+					{quantityDropdown(stock)}
+				</select>
+				<button>Add to Cart</button>
+			</form>
+
+			<button onClick={returnButton}>Return to List</button>
+			<h1>Reviews Placeholder</h1>
+			{/* Create Reviews List component */}
+		</div>
+	);
 }
 
 function returnButton(event) {
-    history.pushState('/product-list'); 
+	history.pushState('/product-list');
 }
 
 function handleSubmit(event) {
-    event.preventDefault(); 
+	event.preventDefault();
 }
 
 function quantityDropdown(stock) {
-    let options = [];
-    for (let i = 1; i < stock+1; i++) {
-        options.push(i); 
-    }
-    return options.map(idx => <option key={idx}>{idx}</option>);
+	let options = [];
+	for (let i = 1; i < stock + 1; i++) {
+		options.push(i);
+	}
+	return options.map(idx => <option key={idx}>{idx}</option>);
 }
 
 /**
  * CONTAINER
  */
 const mapState = (state) => {
-    //console.log('my state', state.items); 
-    return {
-        items: state.items
-    }
+	//console.log('my state', state.items); 
+	return {
+		items: state.items
+	}
 }
 const mapDispatch = (dispatch) => {
-    return {
-        addToCart: function (items) {
-            //dispatch(OUR_THUNKER(items))
-        }
-    }
+	return {
+		addToCart: function (items) {
+			//dispatch(OUR_THUNKER(items))
+		}
+	}
 }
 
 export default connect(mapState, mapDispatch)(SingleProductPage); 
