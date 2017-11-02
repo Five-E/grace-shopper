1160b1bedde2f7260231c7d6be88b2ad856184af
Evans Stepanov <evans@stepanov.email>
2017-11-02 11:44:30 -0400

    feature: db model virtual fields for orders & vscode config

--- server/api/orders.js
+++ server/api/orders.js
@@ -3,7 +3,7 @@ const {Order} = require('../db/models')
 module.exports = router
 
 router.get('/', (req, res, next) => {
-  Order.findAll({ include: [{ all: true, nested: true }]})
+  Order.findAll()
     .then(Orders => res.json(Orders))
     .catch(next)
 })
--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,11 +1,34 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
+const user = require('./user')
+const purchasedItem = require('./purchasedItem')
+const status = require('./status')
+
 const Order = db.define('order', {
-	// subTotal: Sequelize.VIRTUAL,
-	// get: function () {
-	// 	return this.id
-	// }
+	totalPrice: {
+		type: Sequelize.VIRTUAL,
+		get: function () {
+			return this.purchasedItems.reduce((total, item) => {
+				let itemTotal = (item.purchasePrice * item.purchaseQuantity)
+				// need to round due to .00002
+				itemTotal = Math.round(itemTotal * 100) / 100
+				return total + itemTotal
+			}, 0)
+		}
+	},
+	itemQuantity: {
+		type: Sequelize.VIRTUAL,
+		get: function () {
+			return this.purchasedItems.reduce((total, item) => {
+				return total + item.purchaseQuantity
+			}, 0)
+		}
+	}
+}, {
+	defaultScope: {
+		include: [ user, status, purchasedItem ]
+	}
 })
 
 module.exports = Order
