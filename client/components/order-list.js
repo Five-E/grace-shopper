7224d58774f3870944c80e65993db7a76d6f6426
Evans Stepanov <evans@stepanov.email>
2017-11-02 12:31:04 -0400

    feat: front end orders list

---
+++ client/components/order-list.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+
+const OrderList = (props) => {
+
+  const orders = props.orders;
+
+  return (
+    <div>
+      <h3>Orders List Page</h3>
+      {
+        orders.map(order => {
+          return (
+            <div style={{margin: '10px', padding: '15px', border: 'solid 1px black'}} key={order.id}>
+              <span style={{fontWeight: 'bold', display: 'block'}}>order id</span>
+              {order.id}
+              <span style={{fontWeight: 'bold', display: 'block'}}>order by</span>
+              {order.user.name}
+              <span style={{fontWeight: 'bold', display: 'block'}}>order date</span>
+              {order.createdAt}
+              <span style={{fontWeight: 'bold', display: 'block'}}>order total paid</span>
+              {order.totalPrice}
+              <span style={{fontWeight: 'bold', display: 'block'}}>order item count</span>
+              {order.itemQuantity}
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+const mapState = (state) => {
+  return { orders: state.orders }
+}
+
+const mapDispatch = null;
+
+export default connect(mapState, mapDispatch)(OrderList)
--- client/routes.js
+++ client/routes.js
@@ -7,7 +7,9 @@ import history from './history'
 import {Main, Login, Signup, UserHome} from './components'
 import {me} from './store'
 import { fetchItems } from './store/item'
+import { fetchOrders } from './store/orders'
 import ProductList from './components/product-list'
+import OrderList from './components/order-list'
 
 /**
  * COMPONENT
@@ -29,6 +31,7 @@ class Routes extends Component {
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
             <Route path="/product-list" component={ProductList} />
+            <Route path="/order-list" component={OrderList} />
             {
               isLoggedIn &&
                 <Switch>
@@ -59,8 +62,9 @@ const mapState = (state) => {
 const mapDispatch = (dispatch) => {
   return {
     loadInitialData () {
-      dispatch(me());
-      dispatch(fetchItems());
+      dispatch(me())
+      dispatch(fetchItems())
+      dispatch(fetchOrders())
     }
   }
 }
