6cd99cd59eea8133adc4e65207cdd34714ed6290
Eric Garnett <egcodes@gmail.com>
2017-11-03 14:30:17 -0400

    render cart items with quantity

--- client/components/cart.js
+++ client/components/cart.js
@@ -2,24 +2,52 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
 class Cart extends Component {
+  constructor(props) {
+    super(props)
+
+    this.quantityDropdown = this.quantityDropdown.bind(this)
+  }
+
+  quantityDropdown(stock) {
+    let options = []
+    for (let i = 1; i < stock + 1; i++) {
+      options.push(i)
+    }
+    return options.map(idx => <option key={idx}>{idx}</option>)
+
+  }
   render() {
     return (
       <div>
         <h1>This Person's Cart</h1>
-        <img src="#" />
-        <p>Name</p>
-        <p>price</p>
-        <p>quantity</p>
+        {
+          this.props.cart.map((item) => {
+            const targetItem = this.props.items.find(inventory => item.id === inventory.id)
+            return (
+              <div key={item.id}>
+                <img src={item.picture} />
+                <p>{item.name}</p>
+                <p>${item.price}</p>
+                <select>
+                  {
+                    this.quantityDropdown(targetItem.stock)
+                  }
+                </select>
+              </div>
+            )
+          })
+        }
       </div>
 
     )
   }
 }
 
-// const mapState = (state) => {
-//   return {
-//     cartItems: state.cartItems
-//   }
-// }
+const mapState = (state) => {
+  return {
+    cart: state.cart,
+    items: state.items
+  }
+}
 
-export default connect(null, null)(Cart)
+export default connect(mapState, null)(Cart)
