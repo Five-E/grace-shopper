ff3f3c871861118c7ef52b696b3244e3fed7c5c3
Yvette Zhang <yvettez2016@gmail.com>
2017-11-06 11:21:08 -0500

    fix: fixed merging problem

--- server/api/categories.js
+++ server/api/categories.js
@@ -4,10 +4,6 @@ module.exports = router
 
 router.get('/', (req, res, next) => {
   Category.findAll()
-<<<<<<< HEAD
-    .then(categories => res.json(categories))
-=======
     .then(Cat => res.json(Cat))
->>>>>>> 803d19c2156234bbc92619f699a904eaa59a1e95
     .catch(next)
 })
